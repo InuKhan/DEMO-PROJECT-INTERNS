5de55616f4177926a67de77d4463b3742e9a8660
InuKhan <inushakhan123@gmail.com>
2023-01-02 16:10:37 +0530

    Recent change

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import './App.css';
 import Menu from './components/menu'
-
-
-const seeValue = (event) => { 
-  console.log("event ",event);
-  document.getElementById("demo").innerText = event.target.value;
-}
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-
-  let menuItems = [ { id:1, name : "Home", isActive : true },
-                    { id:2, name : "About", isActive : false },
-                    { id:3, name : "Contact Us", isActive : false }
-                  ];
-  let name ="Muzaffar Ahmad";
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <Menu  name={name} menuList={menuItems}/>
-        <p>
-          Hi Folks! we have started our internship learning project
-        </p>
-        <input onChange={seeValue} />  
-        <span>We will see the input value here : <span id="demo"></span> </span>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+     <Menu/>
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
-
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+
+
+
+function Home() {
+
+    return (
+        <>
+          <p>Home Page</p>
+        
+         <img src='https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' width='300px' height='300px' alt='nature'/>
+         <h1>We are on the home page</h1>
+         <div className="background-image" style={{ backgroundImage: "url(https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)" }}></div>
+
+        <p>
+          Hi Folks! we have started our internship learning project
+        </p>
+        </>
+    );
+  }
+  
+  export default Home;
